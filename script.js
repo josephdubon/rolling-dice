df90bc80013ebc216942ea79da037fce12add767
Joseph Dubon <josephdubon@pm.me>
2020-06-12 18:13:04 -0700

    Reflecting results of dice roll through bar graph on index.html.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log("Hello World.")
 count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 // Loop through results 1000 times
-for (let i = 1; i <= 1000; i++) {
+for (let i = 0; i <= 1000; i++) {
 
     // Declare rollOfDice1 and rollOfDice2 and assign them a random number between 1 and 6
     let rollOfDice1
@@ -17,14 +17,16 @@ for (let i = 1; i <= 1000; i++) {
 
     // Save frequency of results to count array
     count[rollOfDice] = count[rollOfDice] + 1;
+}
 
-    // Create a div, with class "bar", and set the width to 75px.
+for (let i = 2; i < count.length; i++) {
+    // Create a div, with class "bar", and set the width to 100px.
     let newElement = document.createElement("div")
     newElement.className = "bar"
-    newElement.style.width = rollOfDice * 75 + "px"
-
+    newElement.style.width = count[i] * 1 + "px"
+    
     // Place a text label inside the new div.
-    let newText = document.createTextNode("Dice Roll #" + i + " " + "Equals " + rollOfDice)
+    let newText = document.createTextNode(i + ": ")
     newElement.appendChild(newText)
 
     // Put the new div on the page inside the existing element "d1".
